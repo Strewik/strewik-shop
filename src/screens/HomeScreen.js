05fd3b2476fdc911ca4faa8490691a39e26ac10b
Strewik <52960396+Strewik@users.noreply.github.com>
2022-09-06 01:20:02 +0300

    Adds Product.js  for product mapping

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Row, Col} from 'react-bootstrap'
+import Product from '../components/Product'
 
 import products from '../products'
 
@@ -9,7 +10,8 @@ const HomeScreen = () => {
         <h1>Latest Products</h1>
         <Row>
             {products.map(product => (
-                <Col>
+                <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                    <Product product={product} />
                 </Col>
             ))}
         </Row>
