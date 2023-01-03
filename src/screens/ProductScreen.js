27f68147ca2319da50a9b897c9905470c13feaca
Strewik <52960396+Strewik@users.noreply.github.com>
2023-01-03 13:32:58 +0300

    added Single product screen

--- src/screens/ProductScreen.js
+++ src/screens/ProductScreen.js
@@ -27,11 +27,35 @@ function ProductScreen(){
               Price: ${product.price}
             </ListGroup.Item>
             <ListGroup.Item>
-              Description: ${product.description}
+              Description: {product.description}
             </ListGroup.Item>
           </ListGroup>
         </Col>
-        <Col md={3}></Col>
+        <Col md={3}>
+          <Card>
+            <ListGroup variant='flush'>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Price:</Col>
+                  <Col>
+                    <strong>${product.price}</strong>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Status:</Col>
+                  <Col>
+                    {product.countInStock > 0 ? 'In stock' : 'Out of stock'}
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Button className="btn-block" disabled={product.countInStock <= 0} type='button'>Add to Cart</Button>
+              </ListGroup.Item>
+            </ListGroup>
+          </Card>
+        </Col>
       </Row>
     </div>
   )
